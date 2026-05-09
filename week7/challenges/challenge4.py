9a2af76cf4fe71bfbed721c55c86c7d7b03a16ba
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 01:59:26 +0200

    Update challenge4.py

--- week7/challenges/challenge4.py
+++ week7/challenges/challenge4.py
@@ -1,7 +1,43 @@
-# challenge4.py — SQL Explorer
-# Present an interactive menu that runs different SQL queries on favorites.db.
-# Requires favorites.db — see week2/README.md for setup instructions.
+library = []
 
-import sqlite3
+while True:
+    print("\n1. Add book")
+    print("2. View books")
+    print("3. Search book")
+    print("4. Exit")
 
-# Your code here
+    choice = input("Choice: ")
+
+    if choice == "1":
+        title = input("Book title: ")
+        author = input("Author: ")
+
+        library.append({
+            "title": title,
+            "author": author
+        })
+
+    elif choice == "2":
+        print("\nLibrary:")
+
+        for book in library:
+            print(f"{book['title']} by {book['author']}")
+
+    elif choice == "3":
+        search = input("Search title: ").lower()
+
+        found = False
+
+        for book in library:
+            if search in book["title"].lower():
+                print(f"Found: {book['title']} by {book['author']}")
+                found = True
+
+        if not found:
+            print("Book not found")
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid choice")
