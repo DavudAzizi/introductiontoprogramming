33d2029a4ac79afd7eee11c56bdcd96c1a8b23b3
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 01:38:36 +0200

    Update recover.c

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,1 +1,59 @@
-week4/recover/recover.c
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    FILE *card = fopen(argv[1], "r");
+
+    if (card == NULL)
+    {
+        printf("Could not open %s.\n", argv[1]);
+        return 1;
+    }
+
+    BYTE buffer[512];
+    FILE *img = NULL;
+    int count = 0;
+    char filename[8];
+
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        if (buffer[0] == 0xff &&
+            buffer[1] == 0xd8 &&
+            buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            sprintf(filename, "%03i.jpg", count);
+            img = fopen(filename, "w");
+            count++;
+        }
+
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+
+    fclose(card);
+
+    return 0;
+}
