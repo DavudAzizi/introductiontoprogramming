694bcea4fd53f858bfac456b0df00323d6e580d4
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 02:33:23 +0200

    Update app.py

--- week10/hello9/app.py
+++ week10/hello9/app.py
@@ -1,5 +1,3 @@
-# Uses a single route
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,5 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name", "world"))
+        name = request.form.get("name", "world")
+        return render_template("greet.html", name=name)
+
     return render_template("index.html")
