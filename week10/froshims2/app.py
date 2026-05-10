3e90e4c38f421e515d9a61d190443fe176a432d9
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 02:37:35 +0200

    Update app.py

--- week10/froshims2/app.py
+++ week10/froshims2/app.py
@@ -1,5 +1,3 @@
-# Implements a registration form using radio buttons
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -18,10 +16,10 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
 
-    # Validate submission
-    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
 
-    # Confirm registration
     return render_template("success.html")
