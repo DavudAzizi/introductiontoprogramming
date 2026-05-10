e621a5a42cea930517011e036356adaa14f3e6d6
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 02:27:54 +0200

    Update app.py

--- week10/hello0/app.py
+++ week10/hello0/app.py
@@ -1,5 +1,3 @@
-# Says hello to world by returning a string of text
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, world"
+    return "Hello, world!"
