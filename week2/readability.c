71ef9a405064ea16f6c5a0ee339bc5efb793fb05
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 02:57:15 +0200

    Refactor readability.c for improved clarity

--- week2/readability.c
+++ week2/readability.c
@@ -3,17 +3,46 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-int main(void) {
-    string s = get_string("Text: ");
-    int l = 0, w = 1, sn = 0;
-    for (int i = 0, n = strlen(s); i < n; i++) {
-        if (isalpha(s[i])) l++;
-        else if (isspace(s[i])) w++;
-        else if (s[i] == '.' || s[i] == '!' || s[i] == '?') sn++;
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
     }
-    float L = (float)l / w * 100, S = (float)sn / w * 100;
+
+    float L = ((float) letters / words) * 100;
+    float S = ((float) sentences / words) * 100;
+
     int index = round(0.0588 * L - 0.296 * S - 15.8);
-    if (index < 1) printf("Before Grade 1\n");
-    else if (index >= 16) printf("Grade 16+\n");
-    else printf("Grade %i\n", index);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
