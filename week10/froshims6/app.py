19a7f09c70492b2f7122f79f6af43d3831a311b6
Davud Azizi <davud.azizi@ibu.edu.mk>
2026-05-10 02:47:21 +0200

    Update app.py

--- week10/froshims6/app.py
+++ week10/froshims6/app.py
@@ -1,5 +1,3 @@
-# Implements a registration form, storing registrants in a SQLite database, with support for deregistration
-
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request
 
@@ -19,35 +17,29 @@ def index():
     return render_template("index.html", sports=SPORTS)
 
 
-@app.route("/deregister", methods=["POST"])
-def deregister():
-
-    # Forget registrant
-    id = request.form.get("id")
-    if id:
-        db.execute("DELETE FROM registrants WHERE id = ?", id)
-    return redirect("/registrants")
-
-
 @app.route("/register", methods=["POST"])
 def register():
-
-    # Validate name
     name = request.form.get("name")
+    sport = request.form.get("sport")
+
     if not name:
         return render_template("error.html", message="Missing name")
 
-    # Validate sport
-    sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
     if sport not in SPORTS:
         return render_template("error.html", message="Invalid sport")
 
-    # Remember registrant
-    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
+    db.execute("INSERT INTO registrants (name, sport) VALUES (?, ?)", name, sport)
 
-    # Confirm registration
+    return redirect("/registrants")
+
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+    id = request.form.get("id")
+
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+
     return redirect("/registrants")
 
 
